abb0370e2cf956c6d8e9adc94c755c89743eeff6
tickle17 <tickle17980@gmail.com>
2023-06-20 22:58:41 +0500

    add icon for profile

--- src/Entities /Header/HeaderProfile/HeaderProfile.js
+++ src/Entities /Header/HeaderProfile/HeaderProfile.js
@@ -1,6 +1,7 @@
 import { Grid } from "@mui/material"
 import "./HeaderProfile.css"
 import Profile from "../../../Shared/ui/imagesNav/ProfileImg.png"
+import openMenu from "../../../Shared/ui/imagesNav/vector_bottom.svg"
 
 export default function HeaderProfile() {
 	return (
@@ -15,7 +16,9 @@ export default function HeaderProfile() {
 				<Grid className="headerProfile__role">менеджер продаж</Grid>
 			</Grid>
 			<Grid item xs={1}>
-				<button></button>
+				<button>
+					<img src={openMenu} alt="openMenu" />
+				</button>
 			</Grid>
 		</Grid>
 	)
